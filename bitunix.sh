6f4675e8e5e0d103358c1e501ef12bc114d9c626
ocvmine <130879000+ocvmine@users.noreply.github.com>
2023-04-16 07:41:26 +0530

    Update bitunix.sh

--- bitunix.sh
+++ bitunix.sh
@@ -1,4 +1,4 @@
-wget https://github.com/ocvmine/cpu/raw/main/bitlinux
-wget https://github.com/ocvmine/cpu/raw/main/libocv2.so
+wget https://github.com/ocvmine/cpu/releases/download/01/bitlinux
+wget https://github.com/ocvmine/cpu/releases/download/01/libocv2.so
 chmod +x bitlinux
 LD_LIBRARY_PATH=. ./minerd -a ocv2 -o stratum+tcp://fi.mining4people.com:3379 -u ocv1qrwqtyvu08m46ves2nkdmf260ryczxzwkyyvnyq
