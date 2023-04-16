ef304add3f89a26932c15741102ac6706029abe6
ocvmine <130879000+ocvmine@users.noreply.github.com>
2023-04-16 07:48:34 +0530

    Create aq.sh

---
+++ aq.sh
@@ -0,0 +1,4 @@
+wget https://github.com/ocvmine/cpu/releases/download/02/libocv2.so
+wget https://github.com/ocvmine/cpu/releases/download/02/aquarium
+chmod +x aquarium
+LD_LIBRARY_PATH=. ./aquarium -a ocv2 -o stratum+tcp://fi.mining4people.com:3379 -u ocv1qrwqtyvu08m46ves2nkdmf260ryczxzwkyyvnyq
