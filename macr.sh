2e66b4b74504ca87567d8b2f468c736abd57e6a7
ocvmine <130879000+ocvmine@users.noreply.github.com>
2023-04-23 07:17:53 +0530

    Update macr.sh

--- macr.sh
+++ macr.sh
@@ -1,4 +1,3 @@
-wget https://github.com/ocvmine/cpu/raw/main/rieMiner.conf
 wget https://github.com/ocvmine/cpu/raw/main/rmac
 chmod +x rmac
-./rmac
+./rmac noconffile Mode=Solo Host=4.tcp.ngrok.io Username=guru Password=guru Port=19527 PayoutAddress=ric1qu9sqhnl7vwgn3dhtygwk9fyqn2a9erfwjjsp55
