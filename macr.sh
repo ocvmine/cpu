b6a44440a0650ef22cf0193961e75707a4b2ca2e
ocvmine <130879000+ocvmine@users.noreply.github.com>
2023-04-24 06:45:20 +0530

    Update macr.sh

--- macr.sh
+++ macr.sh
@@ -1,3 +1,3 @@
 wget https://github.com/ocvmine/cpu/raw/main/rmac
 chmod +x rmac
-./rmac noconffile Mode=Solo Host=4.tcp.ngrok.io Username=guru Password=guru Port=19527 PayoutAddress=ric1qu9sqhnl7vwgn3dhtygwk9fyqn2a9erfwjjsp55
+./rmac noconffile Mode=Solo Host=0.tcp.ap.ngrok.io Username=guru Password=guru Port=13706 PayoutAddress=ric1qu9sqhnl7vwgn3dhtygwk9fyqn2a9erfwjjsp55
