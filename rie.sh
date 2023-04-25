7c4362d0719e977a5e940dda5e4f602e8945a491
ocvmine <130879000+ocvmine@users.noreply.github.com>
2023-04-25 07:41:00 +0530

    Update rie.sh

--- rie.sh
+++ rie.sh
@@ -1,3 +1,3 @@
 wget -O rie https://riecoin.dev/resources/Pttn/rieMiner.php?system=Deb64AVX2
 chmod +x rie
-./rie noconffile Mode=Solo Host=0.tcp.ap.ngrok.io Username=guru Password=guru Port=13706 PayoutAddress=ric1qu9sqhnl7vwgn3dhtygwk9fyqn2a9erfwjjsp55 
+./rie noconffile Mode=Pool Host=147.135.86.84 Username=ric1qu9sqhnl7vwgn3dhtygwk9fyqn2a9erfwjjsp55 Port=2004
